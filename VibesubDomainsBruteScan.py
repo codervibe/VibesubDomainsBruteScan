fcf044af97b7d31aec75453124d727f11db38169
codervibe <3507557934@qq.com>
2024-06-29 18:08:33 +0800

    更新代码

--- VibesubDomainsBruteScan.py
+++ VibesubDomainsBruteScan.py
@@ -1,119 +1,130 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
+"""
+    subDomainsBrute 1.5
+    A simple and fast sub domains brute tool for pentesters
+    my[at]lijiejie.com (http://www.lijiejie.com)
+"""
 
 import sys
-import asyncio
+import multiprocessing
 import time
 import signal
 import os
 import glob
 import shutil
 import platform
-from concurrent.futures import ProcessPoolExecutor
-from multiprocessing import Manager, Lock
 from lib.cmdline import parse_args
-from lib.scanner_py3 import SubNameBrute
-from lib.common_py3 import load_dns_servers, load_next_sub, print_msg, get_out_file_name, \
-    user_abort, wildcard_test, get_sub_file_path
-import logging
 
-# 配置日志
-log_file = 'scan.log'
-logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(message)s', handlers=[
-    logging.FileHandler(log_file),
-    logging.StreamHandler()
-])
 
-# 配置常量
-MAX_THREADS = 1000 if platform.system() != 'Windows' else 200
+import warnings
+warnings.simplefilter("ignore", category=UserWarning)
+max_threads = 1000
 
-def run_process(params):
+if sys.version_info.major >= 3 and sys.version_info.minor >= 5:
+    import asyncio
+    from lib.scanner_py3 import SubNameBrute
+    from lib.common_py3 import load_dns_servers, load_next_sub, print_msg, get_out_file_name, \
+        user_abort, wildcard_test, get_sub_file_path
+    if platform.system() == 'Windows':
+        if sys.version_info.minor >= 8 and hasattr(asyncio, 'WindowsProactorEventLoopPolicy'):
+            asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
+        else:
+            if hasattr(asyncio, 'WindowsSelectorEventLoopPolicy'):
+                asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+            max_threads = 200
+else:
+    from lib.scanner_py2 import SubNameBrute
+    from lib.common_py2 import load_dns_servers, load_next_sub, print_msg, get_out_file_name, \
+        user_abort, wildcard_test, get_sub_file_path
+    if platform.system() == 'Windows':
+        max_threads = 200
+
+
+def run_process(*params):
     signal.signal(signal.SIGINT, user_abort)
     s = SubNameBrute(*params)
     s.run()
 
-def setup_logging():
-    logger = logging.getLogger()
-    handler = logging.FileHandler('scan.log')
-    formatter = logging.Formatter('[%(levelname)s] %(message)s')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    return logger
 
-async def main():
+if __name__ == '__main__':
     options, args = parse_args()
-    if options.threads > MAX_THREADS:
-        options.threads = MAX_THREADS
-
-    # 创建临时目录
+    if options.threads > max_threads:
+        options.threads = max_threads
+    print('''[+] SubDomainsBrute v1.5  https://github.com/lijiejie/subDomainsBrute''')
+    # make tmp dirs
     root_path = os.path.dirname(os.path.abspath(__file__))
-    tmp_dir = os.path.join(root_path, f'tmp/{args[0]}_{int(time.time())}')
-    os.makedirs(tmp_dir, exist_ok=True)
+    tmp_dir = os.path.join(root_path, 'tmp/%s_%s' % (args[0], int(time.time())))
+    if not os.path.exists(tmp_dir):
+        os.makedirs(tmp_dir)
 
+    multiprocessing.freeze_support()
     dns_servers = load_dns_servers()
     next_subs = load_next_sub(options.full_scan)
-
-    manager = Manager()
-    scan_count = manager.Value('i', 0)
-    found_count = manager.Value('i', 0)
-    queue_size_array = manager.list([0] * options.process)
-    lock = Lock()
+    scan_count = multiprocessing.Value('i', 0)
+    found_count = multiprocessing.Value('i', 0)
+    queue_size_array = multiprocessing.Array('i', options.process)
 
     try:
-        logging.info('Running wildcard test')
-        domain = wildcard_test(args[0], dns_servers) if not options.w else args[0]
+        print('[+] Run wildcard test')
+        if not options.w:
+            domain = wildcard_test(args[0], dns_servers)
+        else:
+            domain = args[0]
         options.file = get_sub_file_path(options)
-        logging.info(f'Starting {options.process} scan processes')
-        logging.info('Please wait while scanning ...')
-
+        print('[+] Start %s scan process' % options.process)
+        print('[+] Please wait while scanning ... \n')
         start_time = time.time()
+        all_process = []
+        for process_num in range(options.process):
+            p = multiprocessing.Process(
+                target=run_process,
+                args=(domain, options, process_num, dns_servers, next_subs,
+                      scan_count, found_count, queue_size_array, tmp_dir)
+            )
+            all_process.append(p)
+            p.start()
 
-        # 异步扫描
-        loop = asyncio.get_running_loop()
-        with ProcessPoolExecutor(max_workers=options.process) as executor:
-            tasks = [
-                loop.run_in_executor(
-                    executor, run_process,
-                    (domain, options, process_num, dns_servers, next_subs, scan_count, found_count, queue_size_array, tmp_dir, lock)
-                ) for process_num in range(options.process)
-            ]
-
-            await asyncio.gather(*tasks)
-
-            for count in range(len(tasks)):
-                groups_count = sum(queue_size_array)
-                msg = f'[{"/-\\|"[count % 4]}] {found_count.value} found, {scan_count.value} scanned in {time.time() - start_time:.1f} seconds, {groups_count} groups left'
-                logging.info(msg)
-                await asyncio.sleep(0.3)
-
-    except KeyboardInterrupt:
-        logging.error('User aborted the scan!')
+        char_set = ['\\', '|', '/', '-']
+        count = 0
+        while all_process:
+            for p in all_process:
+                if not p.is_alive():
+                    all_process.remove(p)
+            groups_count = 0
+            for c in queue_size_array:
+                groups_count += c
+            msg = '[%s] %s found, %s scanned in %.1f seconds, %s groups left' % (
+                char_set[count % 4], found_count.value, scan_count.value, time.time() - start_time, groups_count)
+            print_msg(msg)
+            count += 1
+            time.sleep(0.3)
+    except KeyboardInterrupt as e:
+        print('[ERROR] User aborted the scan!')
+        for p in all_process:
+            p.terminate()
     except Exception as e:
-        logging.error(f'Error: {str(e)}', exc_info=True)
-    finally:
-        # 确保临时目录被清理
-        try:
-            shutil.rmtree(tmp_dir)
-        except Exception as e:
-            logging.warning(f'Failed to remove tmp dir: {e}')
+        import traceback
+        traceback.print_exc()
+        print('[ERROR] %s' % str(e))
 
     out_file_name = get_out_file_name(domain, options)
     all_domains = set()
     domain_count = 0
-
     with open(out_file_name, 'w') as f:
-        for _file in glob.glob(os.path.join(tmp_dir, '*.txt')):
+        for _file in glob.glob(tmp_dir + '/*.txt'):
             with open(_file, 'r') as tmp_f:
                 for domain in tmp_f:
                     if domain not in all_domains:
                         domain_count += 1
-                        all_domains.add(domain)
+                        all_domains.add(domain)       # cname query can result in duplicated domains
                         f.write(domain)
 
-    msg = f'All Done. {domain_count} found, {scan_count.value} scanned in {time.time() - start_time:.1f} seconds.'
-    logging.info(msg)
-    logging.info(f'Output file is {out_file_name}')
-
-
-if __name__ == '__main__':
-    asyncio.run(main())
+    msg = 'All Done. %s found, %s scanned in %.1f seconds.' % (
+        domain_count, scan_count.value, time.time() - start_time)
+    print_msg(msg, line_feed=True)
+    print('Output file is %s' % out_file_name)
+    try:
+        shutil.rmtree(tmp_dir)
+    except Exception as e:
+        pass
